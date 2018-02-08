616950820d1f59e0bd0b9191f6c386ef24f22837
katieyves <katya24081992@mail.ru>
2018-02-08 20:27:18 +0300

    set static files for heroku

--- locotravel/settings.py
+++ locotravel/settings.py
@@ -135,7 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 DATABASES = {
